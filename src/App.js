2873efa47e83a49526069f71998ee275abf12796
nwmaker <makernw@gmail.com>
2018-05-07 13:27:21 -0600

    add-ga

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
+  Router,
   Route,
   Link
 } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+//import { slide as Menu } from 'react-burger-menu'
+import Analytics from 'react-router-ga'
 
 import Home from './pages/home'
 import About from './pages/about'
@@ -14,24 +16,6 @@ import Footer from './layout/footer'
 import './App.css'
 
 const history = createBrowserHistory();
-/*
-const initGA = (history) => {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-118045706-1', 'auto');
-  ga('send', 'pageview');
-
-  history.listen((location) => {
-    console.log("tracking page view: " + location.pathname);
-    ga('send', 'pageview', location.pathname);
-  });
-};
-
-initGA(history);
-*/
 
 const CustomMenuLink = ({ label, to, activeOnlyWhenExact }) => (
   <Route
@@ -48,6 +32,7 @@ const CustomMenuLink = ({ label, to, activeOnlyWhenExact }) => (
 export default () => (
   <Router history={history}>
     <div>
+    <Analytics id="UA-118045706-1">
       <div className='nav-container'>
         <CustomMenuLink activeOnlyWhenExact={true} to="/" label="Home" />
         <CustomMenuLink to="/products" label="Products" />
@@ -63,6 +48,7 @@ export default () => (
       <hr />
       
       <Footer />
+    </Analytics>
     </div>
   </Router>
 )
