3012862e86059ddf66319a699ed434bfbc62c557
nwmaker <makernw@gmail.com>
2018-05-15 20:40:12 -0600

    change-about

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,14 +6,14 @@ export default () => (
     <Column flexGrow={1}>
       <Row horizontal='center'>
         <h1>Design studio</h1>
-        <p>
+        <div>
           All beautiful kits designed here may be available in different package.            <ul>
             <li>Kit: for experienced electronics hobbyist</li>
             <li>Half-assembled: for beginner hobbyist to practice soldering of through-hole components</li>
             <li>Fully-assembled: for curious users without soldering experience</li>
           </ul>
           Once you have the kits, either assembled or not, you have all the information about the design, the circuits, and source code in some cases.
-        </p>
+        </div>
       </Row>
     </Column>
   </div>
